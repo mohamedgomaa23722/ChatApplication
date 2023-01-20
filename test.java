817ebd7c7040d1dcd2875ad6f91da772c00af8d0
mohamed gomaa <mohamedgomaa23722@gmail.com>
2023-01-20 15:25:21 +0200

    test5

--- test.java
+++ test.java
@@ -1,12 +1,5 @@
 public class Test {
 	public static void main (String[] args) {
-<<<<<<< HEAD
-		int v = 0;
-		int h = 5;
-=======
-		int v = 20;
-		v=3;
-		System.out.println(v);
->>>>>>> a9411e395eaac6755dbde1baa19d4f3ca9b44c3a
+		int gg =0;
 	}
 }
